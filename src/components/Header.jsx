b437057eae12ad28956ba3bb0162e1c2af62a8e4
MianFaizanAmir0053 <97526215+MianFaizanAmir0053@users.noreply.github.com>
2023-04-02 05:08:09 +0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
+//import styled from "styled-components";
 
 function Header() {
   const [menuState, menuStateFun] = useState(false);
@@ -44,68 +44,68 @@ function Header() {
 
 export default Header;
 
-const Container = styled.div`
-  text-transform: uppercase;
-  padding: 0.5rem 2rem;
-  position: fixed;
-  display: flex;
-  width: 100%;
-  justify-content: space-between;
-  flex-direction: row;
-  font-weight: 600;
-  align-items: center;
-`;
+// const Container = styled.div`
+//   text-transform: uppercase;
+//   padding: 0.5rem 2rem;
+//   position: fixed;
+//   display: flex;
+//   width: 100%;
+//   justify-content: space-between;
+//   flex-direction: row;
+//   font-weight: 600;
+//   align-items: center;
+// `;
 
-const Menu = styled.div`
-  display: flex;
-  flex-direction: row;
+// const Menu = styled.div`
+//   display: flex;
+//   flex-direction: row;
 
-  a {
-    margin: 0 0 0 3rem;
-  }
+//   a {
+//     margin: 0 0 0 3rem;
+//   }
 
-  @media only screen and (max-width: 1000px) {
-    /* For mobile phones: */
-    display: none;
-  }
-`;
+//   @media only screen and (max-width: 1000px) {
+//     /* For mobile phones: */
+//     display: none;
+//   }
+// `;
 
-const RightMenu = styled.div`
-  display: flex;
-  flex-direction: row;
+// const RightMenu = styled.div`
+//   display: flex;
+//   flex-direction: row;
 
-  a:first-child {
-    margin-right: 1rem;
-  }
-  img {
-    margin-left: 10px;
-    width: 18px;
-  }
-`;
+//   a:first-child {
+//     margin-right: 1rem;
+//   }
+//   img {
+//     margin-left: 10px;
+//     width: 18px;
+//   }
+// `;
 
-const Hamburger = styled.div`
-  top: 0;
-  right: 0;
-  width: 20rem;
-  padding-left: 3rem;
-  padding-top: 2rem;
-  position: fixed;
-  display: flex;
-  flex-direction: column;
-  background-color: white;
-  height: 100vh;
-  font-weight: 100;
-  transform: translateX(100%);
-  transform: ${(props) => (props.show ? "translateX(0)" : "trasnlateX(100%)")};
-  transition: all 0.2s;
-  a {
-    padding: 0.7rem 0;
-  }
-  img {
-    width: 2.5rem;
-  }
-  span {
-    display: flex;
-    justify-content: right;
-  }
-`;
+// const Hamburger = styled.div`
+//   top: 0;
+//   right: 0;
+//   width: 20rem;
+//   padding-left: 3rem;
+//   padding-top: 2rem;
+//   position: fixed;
+//   display: flex;
+//   flex-direction: column;
+//   background-color: white;
+//   height: 100vh;
+//   font-weight: 100;
+//   transform: translateX(100%);
+//   transform: ${(props) => (props.show ? "translateX(0)" : "trasnlateX(100%)")};
+//   transition: all 0.2s;
+//   a {
+//     padding: 0.7rem 0;
+//   }
+//   img {
+//     width: 2.5rem;
+//   }
+//   span {
+//     display: flex;
+//     justify-content: right;
+//   }
+// `;
